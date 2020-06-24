352cf180a786db5cebd6ff341b36401b8334de6b
alex-whan <whan.alexander@gmail.com>
2020-06-23 18:13:57 -0700

    Refactored jQuery for add button and tweaked collection, detail, and header

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,12 @@
 'use strict';
 
+// $('.preState').hide();
+
 function hideAddButton(event) {
+    event.preventDefault();
     $('#addButton').hide();
+    $('.preState').show();
+
 }
 
-$('#addButton').on('click', hideAddButton)
+// $('#addButton').on('click', hideAddButton);
